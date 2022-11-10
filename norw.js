bb1abe606cf82289ff85efd854c09b357958541a
Nórw <81948640+sa594@users.noreply.github.com>
2022-11-10 11:26:58 +0400

    Create norw.js

---
+++ norw.js
@@ -0,0 +1,32 @@
+const { Client, GatewayIntentBits, Partials } = require("discord.js");
+const ayarlar = require("./ayarlar.json");
+
+const client = new Client({
+  intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.GuildPresences,
+      GatewayIntentBits.GuildMessageReactions,
+      GatewayIntentBits.DirectMessages,
+      GatewayIntentBits.MessageContent
+  ],
+  partials: [Partials.Channel, Partials.Message, Partials.User, Partials.GuildMember, Partials.Reaction]
+});
+
+module.exports = client;
+
+require("./events/message.js")
+require("./events/ready.js")
+
+const express = require("express");
+const app = express();
+const http = require("http");
+app.get("/", (request, response) => {
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+
+client.login(process.env.TOKEN || ayarlar.token)
+
+//Botunuzun Tokenini Glitch kullanıyorsanız .env'ye, Eğer Vds Kullanıyorsanız ayarlar.json'a yapıştırın
+//Nórw#0001
